22d951b57fbbc7b528f8dddf1af3b3e7446b38e7
Abdubosit01 <abdubosit0115@gmail.com>
2025-08-06 14:44:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+let a =  document.querySelector('p')
+
+
+let b = prompt ('text rangi')
+
+
+a.style.color = b
+
+
+
+let w = document.querySelector('body')
+
+
+let p = prompt ('Orqa fon rangi')
+
+
+
+w.style.background = p
